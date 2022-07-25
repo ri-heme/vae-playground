151cef3940e4725417e74399f62933ede0fbc55a
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-07-25 13:58:49 +0200

    :art: Set up local imports

--- src/vaeplayland/__init__.py
+++ src/vaeplayland/__init__.py
@@ -1,2 +1,4 @@
 __version__ = (0, 0, 0)
-__all__ = []
+__all__ = ["models"]
+
+from vaeplayland import models
---
+++ src/vaeplayland/models/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["VAE", "encoders", "decoders"]
+
+from vaeplayland.models.vae import VAE
+from vaeplayland.models import encoders, decoders
