0270688f35233d3929399dd258e719f811b8d03e
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-07-28 11:16:09 +0200

    :sparkles: Add PyTorch Lightning VAE module

--- src/vaeplayland/models/loss.py
+++ src/vaeplayland/models/loss.py
@@ -1,6 +1,6 @@
 __all__ = ["compute_elbo"]
 
-from typing import Optional
+from typing import Dict, Optional
 
 import torch
 from torch import nn
@@ -11,7 +11,7 @@ def compute_kl_div(z: torch.Tensor, qz_loc: torch.Tensor, qz_scale: torch.Tensor
     """Computes the KL divergence between posterior q(z|x) and prior p(z). The
     prior has a Normal(0, 1) distribution."""
     qz = Normal(qz_loc, qz_scale)
-    pz = Normal(0, 1)
+    pz = Normal(0.0, 1.0)
     kl_div: torch.Tensor = qz.log_prob(z) - pz.log_prob(z)
     return kl_div.sum(-1)
 
@@ -21,18 +21,29 @@ def compute_gaussian_log_prob(
 ) -> torch.Tensor:
     """Computes the log of the probability density of the likelihood p(x|z)."""
     if px_scale is None:
-        px_scale = torch.ones(1)
+        px_scale = 1.0
     px = Normal(px_loc, px_scale)
     log_px: torch.Tensor = px.log_prob(x)
     return log_px.sum(dim=[*range(1, log_px.dim())])
 
 
-def compute_elbo(model: nn.Module, batch: torch.Tensor) -> torch.Tensor:
+def compute_elbo(
+    model: nn.Module,
+    batch: torch.Tensor,
+    kl_weight: float = 1.0,
+    annealing_factor: float = 1.0
+) -> Dict[str, torch.Tensor]:
     """Computes the evidence lower bound objective."""
     x, _ = batch
     px_loc, z, qz_loc, qz_logvar = model(batch)
     qz_scale = (0.5 * qz_logvar).exp()
-    reg_loss = compute_kl_div(z, qz_loc, qz_scale)
-    rec_loss = compute_gaussian_log_prob(x, px_loc)
-    elbo = (reg_loss - rec_loss).mean()
-    return elbo
+    reg_loss = compute_kl_div(z, qz_loc, qz_scale).mean()
+    rec_loss = compute_gaussian_log_prob(x, px_loc).mean()
+    kl_weight = kl_weight * annealing_factor
+    elbo = kl_weight * reg_loss - rec_loss
+    return dict(
+        elbo=elbo,
+        reg_loss=reg_loss,
+        rec_loss=rec_loss,
+        kl_weight=kl_weight,
+    )
--- src/vaeplayland/models/vae.py
+++ src/vaeplayland/models/vae.py
@@ -1,9 +1,16 @@
-from typing import Tuple
+__all__ = ["VAE", "LightningVAE"]
+
+import math
+from typing import Dict, Tuple
 
 import torch
-from torch import nn
-from torch.autograd import Variable
+from torch import nn, optim
 from torch.distributions import Normal
+
+import pytorch_lightning as pl
+from pytorch_lightning.trainer.states import RunningStage
+
+from vaeplayland.models.loss import compute_elbo
 
 
 class VAE(nn.Module):
@@ -23,3 +30,61 @@ class VAE(nn.Module):
         z = self.reparametrize(mu, logvar)
         recon = self.decoder(z)
         return recon, z, mu, logvar
+
+
+class LightningVAE(pl.LightningModule):
+    def __init__(
+        self,
+        vae: "VAE",
+        kl_weight: float = 1.0,
+        annealing_epochs: int = 20,
+        annealing_schedule: str = "linear",
+    ) -> None:
+        super().__init__()
+        self.vae = vae
+        self.save_hyperparameters(ignore="vae")
+
+    @property
+    def annealing_factor(self) -> float:
+        epoch = self.trainer.current_epoch
+        if (
+            self.trainer is not None
+            and self.trainer.state.stage == RunningStage.TRAINING
+            and epoch < self.hparams.annealing_epochs
+        ):
+            if self.hparams.annealing_schedule == "stairs":
+                return epoch / self.hparams.annealing_epochs
+            num_batches = len(self.trainer.train_dataloader)
+            step = self.trainer.global_step
+            slope = 1 / (self.hparams.annealing_epochs * num_batches)
+            if self.hparams.annealing_schedule == "sigmoid":
+                # actual slope is 10 times slope of linear function
+                # shift is half of the annealing epochs
+                # equation below is factorized to avoid repeating terms
+                shift = 0.5
+                return 1 / (1 + math.exp(10 * (shift - step * slope)))
+            elif self.hparams.annealing_schedule == "linear":
+                return step * slope
+        return 1.0
+
+    @property
+    def kl_weight(self) -> float:
+        return self.hparams.kl_weight
+
+    def forward(self, batch: torch.Tensor) -> Tuple[torch.Tensor, ...]:
+        return self.vae(batch)
+
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=1e-4)
+
+    def step(self, batch: torch.Tensor) -> torch.Tensor:
+        output = compute_elbo(self.vae, batch, self.kl_weight, self.annealing_factor)
+        for key, value in output.items():
+            self.log(f"{self.trainer.state.stage}_{key}", value)
+        return output["elbo"]
+
+    def training_step(self, batch: torch.Tensor) -> torch.Tensor:
+        return self.step(batch)
+
+    def validation_step(self, batch: torch.Tensor) -> None:
+        self.step(batch)
