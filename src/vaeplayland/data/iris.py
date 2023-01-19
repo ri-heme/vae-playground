5aed42b2ab5c21830dfafd9b638c17e0c56cffcc
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2023-01-18 19:41:14 -0600

    :sparkles: Add Iris dataset (bimodal)

---
+++ src/vaeplayland/data/iris.py
@@ -0,0 +1,51 @@
+from typing import Any, Literal
+
+import torch
+from torch.utils.data import DataLoader, TensorDataset, Subset
+from sklearn.datasets import load_iris
+
+
+def get_dataloader(
+    split: Literal["train", "test"] = "train",
+    seed: int = 42,
+    test_frac: float = 0.2,
+    **dataloader_kwargs
+) -> DataLoader:
+    """Load Iris dataset and return a training and validation dataloaders.
+
+    Labels are one-hot encoded, and continuous values are standardized. Both
+    data modalities are concatenated.
+    """
+    # Load
+    data = load_iris()
+    features = torch.FloatTensor(getattr(data, "data"))
+    labels = torch.FloatTensor(getattr(data, "target"))
+    num_samples = labels.size(0)
+    num_categories = len(getattr(data, "target_names"))
+
+    # One-hot encode labels
+    ohe_data = torch.zeros((num_samples, num_categories))
+    ohe_data[torch.arange(num_samples), labels.long()] = 1
+
+    # Split train/test
+    generator = torch.Generator().manual_seed(seed)
+    ids = torch.randperm(num_samples, generator=generator)
+    train_size = int(num_samples * (1 - test_frac))
+    train_ids, test_ids = [
+        torch.sort(t).values for t in torch.tensor_split(ids, [train_size])
+    ]
+
+    # Standardize
+    mean = features[train_ids, :].mean(dim=0)
+    scale = features[train_ids, :].std(dim=0)
+
+    features = (features - mean) / scale
+    cat_data = torch.cat((ohe_data, features), dim=1)
+
+    dataset = TensorDataset(cat_data, labels)
+    if split == "train":
+        return DataLoader(Subset(dataset, train_ids), **dataloader_kwargs)
+    elif split == "test":
+        return DataLoader(Subset(dataset, test_ids), **dataloader_kwargs)
+
+    raise ValueError("Unsupported split")
