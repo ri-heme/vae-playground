51197cdbc890ed802032a4ccbb9d59c8aee9c8bb
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2023-02-07 15:26:28 +0100

    :construction: Create experimental bimodal VAE

---
+++ src/vaeplayland/models/experimental/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["ExperimentalBimodalDecoder", "ExperimentalBimodalVAE"]
+
+from vaeplayland.models.experimental.bimodal_vae import (
+    ExperimentalBimodalDecoder,
+    ExperimentalBimodalVAE,
+)
