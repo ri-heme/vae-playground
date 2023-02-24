8bc73c12966dfed0dfb00bfe83e1053a9f9bbd8c
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2023-02-24 15:51:02 +0100

    :sparkles: Add generic dataloader

---
+++ src/vaeplayland/data/generic.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+
+
+def get_dataloader(source_dir: str, split: str = "train", **dataloader_args):
+    # Reload data
+    source_path = Path(source_dir)
+    cat1 = torch.load(source_path / f"cat1_{split}.pt")
+    con = torch.load(source_path / f"con_{split}.pt")
+    # Get labels
+    lab1 = torch.argmax(cat1, dim=1)
+    # Prep dataloader
+    data = torch.cat((cat1, con), dim=0)
+    dataset = TensorDataset(data, lab1)
+    return DataLoader(dataset, **dataloader_args)
