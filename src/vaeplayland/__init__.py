ee91c7125b963dac86933db75d8336bf534e49f1
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2023-07-14 14:27:11 +0200

    :fire: Remove experimental module ref

--- src/vaeplayland/__init__.py
+++ src/vaeplayland/__init__.py
@@ -1,6 +1,5 @@
 __version__ = (0, 0, 0)
-__all__ = ["data", "experimental", "models", "read_config", "training"]
+__all__ = ["data", "models", "read_config", "training"]
 
 from vaeplayland import data, models, training
 from vaeplayland.config import read_config
-from vaeplayland.models import experimental
